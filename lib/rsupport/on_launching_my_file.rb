3c8b809ce2b957671d59a6014a52348bc7cd9bdf
nomuvan <nomuvan@gmail.com>
2014-04-28 23:40:31 +0900

    modify

--- lib/rsupport/on_launching_my_file.rb
+++ lib/rsupport/on_launching_my_file.rb
@@ -1,13 +1,5 @@
 
 module Kernel
-  # def load_path_on_launching_my_file project, paths, file
-  #   return unless $0 == file
-  #   parts = "/#{project}/"
-  #   paths = [paths] if paths.is_a? String
-  #   paths.each do |path|
-  #     $:.unshift file[0,file.index(parts)+parts.size+1]+"/#{path}"
-  #   end
-  # end
 
   def on_launching file, param={}
     return unless $0 == file
@@ -24,4 +16,5 @@ module Kernel
       end
     end
   end
+
 end
