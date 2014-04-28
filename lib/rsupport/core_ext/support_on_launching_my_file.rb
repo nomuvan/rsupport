ef082c8f54c3f10a7dac37bc27b6d0b6e4ab0c62
nomuvan <nomuvan@gmail.com>
2014-04-28 23:29:45 +0900

    modify

--- lib/rsupport/core_ext/support_on_launching_my_file.rb
+++ lib/rsupport/core_ext/support_on_launching_my_file.rb
@@ -1,16 +1,27 @@
 
 module Kernel
-  def load_path_on_launching_my_file project, paths, file
-    return unless $0 == file
-    parts = "/#{project}/"
-    paths = [paths] if paths.is_a? String
-    paths.each do |path|
-      $:.unshift file[0,file.index(parts)+parts.size+1]+"/#{path}"
-    end
-  end
+  # def load_path_on_launching_my_file project, paths, file
+  #   return unless $0 == file
+  #   parts = "/#{project}/"
+  #   paths = [paths] if paths.is_a? String
+  #   paths.each do |path|
+  #     $:.unshift file[0,file.index(parts)+parts.size+1]+"/#{path}"
+  #   end
+  # end
 
-  def exec_on_launching_my_file file
+  def on_launching file, param={}
     return unless $0 == file
-    yield
+    if block_given?
+      yield
+    else
+      paths = param[:load]
+      paths = [paths] if paths.is_a? String
+      param[:project] ||= file.split("/")[file.split("/").rindex{|x|paths.include?(x)}-1]
+      parts = "/#{param[:project]}/"
+      paths.each do |path|
+        puts file[0,file.index(parts)+parts.size-1]+"/#{path}"
+        $:.unshift file[0,file.index(parts)+parts.size-1]+"/#{path}"
+      end
+    end
   end
 end
