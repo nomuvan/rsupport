650145099d679ff28e8ec876874be8a60836b2ea
nomuvan <nomuvan@gmail.com>
2014-04-28 22:43:53 +0900

    modify

--- lib/rsupport/core_ext/support_on_launching_my_file.rb
+++ lib/rsupport/core_ext/support_on_launching_my_file.rb
@@ -1,9 +1,12 @@
 
 module Kernel
-  def load_path_on_launching_my_file project, path, file
+  def load_path_on_launching_my_file project, paths, file
     return unless $0 == file
-    parts = "#{project}/#{path}"
-    $:.unshift file[0,file.index(parts)+parts.size]
+    parts = "/#{project}/"
+    paths = [paths] if paths.is_a? String
+    paths.each do |path|
+      $:.unshift file[0,file.index(parts)+parts.size+1]+"/#{path}"
+    end
   end
 
   def exec_on_launching_my_file file
